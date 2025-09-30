2c715f75f1cb7a2c8f3ea0e681cba18d2a94e4d4
Amir <sefati100@gmail.com>
2025-09-30 21:56:30 +0330

    feat: add first component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,16 @@ function App() {
     <div>
 
       <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+      <div className="flex bg-box flex-row-reverse w-[440px] p-3 rounded-lg">
+        <div className="flex-1 justify-items-center">
+          <img src="https://laletka.com/wp-content/uploads/2025/05/new-collection.png" alt="" />
+        </div>
+        <div className="flex-1 text-center justify-items-center pt-5">
+          <strong>new collection</strong> <br />
+          <small>کالکشن جدید</small>
+        </div>
 
+      </div>
     </div>
   )
 }
