c4c38b71509d4c9faeea1f796ab999f8a8312e36
Amir <sefati100@gmail.com>
2025-10-01 14:17:59 +0330

    feat: rtl, fa

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,42 @@ import { CiInstagram } from "react-icons/ci";
 import { FaTelegramPlane } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
 
-
-
-
-
 function App() {
 
   return (
-    <div>
+    <div lang="fa" style={{ 'direction': 'rtl' }}>
 
       <header className="flex flex-row-reverse justify-around m-3">
-        <div><img src="https://laletka.com/wp-content/uploads/2022/05/laletka-brand-Horz.webp"  className="w-[200px]"  alt="logo of brand" /></div>
+        <div><img src="https://laletka.com/wp-content/uploads/2022/05/laletka-brand-Horz.webp" className="w-[200px]" alt="logo of brand" /></div>
         <div className="flex gap-2.5 flex-row-reverse">
-           <input type="text" placeholder="جستجوی محصولات" className="text-right rounded-4xm bg-[#ebebebd5] w-[300px] h-[50px]"   />
+          <button className="flex items-center gap-2 bg-black text-amber-50 px-3 py-1 rounded-md hover:bg-gray-800 transition cursor-pointer">
+            <SlBasket className="w-5 h-5" />
+            <span>سبد خرید</span>
+          </button>
+          <input type="text" placeholder="جستجوی محصولات" className="text-right rounded-4xm bg-[#ebebebd5] w-[300px] h-[50px]" />
 
-           <button className="bg-black text-amber-50 h-[50px] p-1 cursor-grab rounded-4xl">ورود / عضویت</button>
-           <button className="bg-black text-amber-50  p-1 cursor-grab w-[]"> <SlBasket/>سبد خرید</button>
+          <button className="bg-black text-amber-50 h-[50px] p-1 cursor-grab rounded-4xl">ورود / عضویت</button>
+
+
+
         </div>
       </header>
-      <nav className="flex justify-between bg-[#ebebebd5]"> 
-      <div className="flex">
-        <ul>
-          <li>جستجوی محصولات</li>
-          <li>فروشگاه</li>
-          <li>وبلاگ</li>
-          <li>تخفیف</li>
-          <li>درباره ما</li>
-          <li>تماس با ما</li>
-        </ul>
-      </div>
-      <div>
-        <CiInstagram/>?
-        <img src={ FaTelegramPlane }  alt="" />
-        <img src={ FaWhatsapp } alt="" />
-      </div>
+      <nav className="flex justify-between bg-[#ebebebd5]">
+        <div >
+          <ul className="flex gap-3">
+            <li>جستجوی محصولات</li>
+            <li>فروشگاه</li>
+            <li>وبلاگ</li>
+            <li>تخفیف</li>
+            <li>درباره ما</li>
+            <li>تماس با ما</li>
+          </ul>
+        </div>
+        <div>
+          <CiInstagram />?
+          <img src={FaTelegramPlane} alt="" />
+          <img src={FaWhatsapp} alt="" />
+        </div>
       </nav>
       {/* <div className="flex bg-box flex-row-reverse w-[440px] p-3 rounded-lg">
         <div className="flex-1 justify-items-center">
